dbfc220642e754d8fc8298224ee746698b6e2e2a
mattn <mattn.jp@gmail.com>
2013-05-29 15:03:49 +0900

    Support URL

--- lingr-test.go
+++ lingr-test.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -37,51 +38,59 @@ type Message struct {
 	Mine            bool   `json:"mine"`
 }
 
+var reUrl = regexp.MustCompile(`(?:^|[^a-zA-Z0-9])(https?://[a-zA-Z][a-zA-Z0-9_-]*(\.[a-zA-Z0-9][a-zA-Z0-9_-]*)*(:\d+)?(?:/[a-zA-Z0-9_/.\-+%#?&=;@$,!*~]*)?)`)
+
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "usage: lingr-test [bot_id] [text]")
+		fmt.Fprintln(os.Stderr, "usage: lingr-test [options] [bot_id|URL] [text]")
+		flag.PrintDefaults()
+		return
 	}
-	uri := fmt.Sprintf("http://lingr.com/bot/%s", os.Args[1])
-	doc, err := goquery.NewDocument(uri)
+
+	uri := os.Args[1]
+	if !reUrl.MatchString(uri) {
+		uri = fmt.Sprintf("http://lingr.com/bot/%s", os.Args[1])
+		doc, err := goquery.NewDocument(uri)
+		if err != nil {
+			log.Fatal(err)
+		}
+		uri = ""
+		doc.Find("#property .left").Each(func(_ int, s *goquery.Selection) {
+			if strings.TrimSpace(s.Text()) == "Endpoint:" {
+				uri = strings.TrimSpace(s.Next().Text())
+			}
+		})
+		if uri == "" {
+			log.Fatal("404 Bot Not Found")
+		}
+	}
+
+	events := map[string][]Event{
+		"events": {
+			{
+				Id: 12345,
+				Message: &Message{
+					Id:              "",
+					Room:            *room,
+					PublicSessionId: "VIM",
+					IconUrl:         *icon_url,
+					Type:            *ptype,
+					SpeakerId:       *speaker_id,
+					Nickname:        *nickname,
+					Text:            strings.Join(os.Args[2:], " "),
+					Timestamp:       time.Now().Format(time.RFC822),
+				},
+			},
+		},
+	}
+	b, err := json.Marshal(&events)
 	if err != nil {
 		log.Fatal(err)
 	}
-	uri = ""
-	doc.Find("#property .left").Each(func(_ int, s *goquery.Selection) {
-		if strings.TrimSpace(s.Text()) == "Endpoint:" {
-			uri = strings.TrimSpace(s.Next().Text())
-		}
-	})
-	if uri == "" {
-		log.Fatal("404 Bot Not Found")
-	} else {
-		events := map[string][]Event{
-			"events": {
-				{
-					Id: 12345,
-					Message: &Message{
-						Id:              "",
-						Room:            *room,
-						PublicSessionId: "VIM",
-						IconUrl:         *icon_url,
-						Type:            *ptype,
-						SpeakerId:       *speaker_id,
-						Nickname:        *nickname,
-						Text:            strings.Join(os.Args[2:], " "),
-						Timestamp:       time.Now().Format(time.RFC822),
-					},
-				},
-			},
-		}
-		b, err := json.Marshal(&events)
-		if err != nil {
-			log.Fatal(err)
-		}
-		r, err := http.Post(uri, "application/json", strings.NewReader(string(b)))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer r.Body.Close()
-		io.Copy(os.Stdout, r.Body)
+	r, err := http.Post(uri, "application/json", strings.NewReader(string(b)))
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer r.Body.Close()
+	io.Copy(os.Stdout, r.Body)
 }
